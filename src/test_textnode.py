ae09810cf6b6ea287d2d80fb077d21e2db0832ec
jkk290 <jared@kuromoto.dev>
2025-10-03 07:02:15 -1000

    Add function for converting text to html nodes and unit test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -27,6 +27,25 @@ class TestTextNode(unittest.TestCase):
             "TextNode(test repr, text, None)",
             repr(node)
         )
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node  = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_link_to_html(self):
+        node = TextNode("click me", TextType.LINK, "https://link.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "click me")
+        self.assertEqual(html_node.props, {"href": "https://link.com"})
+    
+    def test_img_to_html(self):
+        node = TextNode("an image", TextType.IMAGE, "https://link.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.props, {"src": "https://link.com", "alt": "an image"})
 
 if __name__ == "__main__":
     unittest.main()
