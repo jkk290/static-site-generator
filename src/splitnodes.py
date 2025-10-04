01d575505c05ed577bfc384cab082a8a07a56902
jkk290 <jared@kuromoto.dev>
2025-10-04 06:55:59 -1000

    Add function for combining all split node functions

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -79,3 +79,12 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(remaining, TextType.TEXT))
 
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    new_nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+    new_nodes = split_nodes_link(new_nodes)
+    new_nodes = split_nodes_image(new_nodes)
+    return new_nodes
--- src/test_splitdelimiter.py
+++ src/test_splitdelimiter.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from splitnodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
+from splitnodes import split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestSplit(unittest.TestCase):
     def test_splitdelimiter(self):
@@ -104,6 +104,23 @@ class TestSplit(unittest.TestCase):
                 ),
             ],
             new_nodes,
+        )
+    
+    def test_text_to_textnodes(self):
+        new_nodes = text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ], new_nodes
         )
 
 if __name__ == "__main__":
