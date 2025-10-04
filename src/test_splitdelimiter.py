9958e44c50d3469cc732ec7a402141bac3196188
jkk290 <jared@kuromoto.dev>
2025-10-03 21:00:53 -1000

    Add function for splitting images and unit test

--- src/test_splitdelimiter.py
+++ src/test_splitdelimiter.py
@@ -1,9 +1,9 @@
 import unittest
 
 from textnode import TextNode, TextType
-from splitdelimiter import split_nodes_delimiter
+from splitnodes import split_nodes_delimiter, split_nodes_image
 
-class TestSplitDelimiter(unittest.TestCase):
+class TestSplit(unittest.TestCase):
     def test_splitdelimiter(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
@@ -41,6 +41,24 @@ class TestSplitDelimiter(unittest.TestCase):
         node = TextNode("this is **** empty", TextType.TEXT)
         with self.assertRaises(Exception):
             split_nodes_delimiter([node], "**", TextType.BOLD)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
 
 if __name__ == "__main__":
     unittest.main()
