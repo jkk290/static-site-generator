753b6115812037de6b1ded13281b3df34359f39d
jkk290 <jared@kuromoto.dev>
2025-10-05 10:01:14 -1000

    Add generate page recursive function

--- src/generatepage.py
+++ src/generatepage.py
@@ -17,3 +17,34 @@ def generate_page(from_path, template_path, dest_path):
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     with open(dest_path, "w") as f: f.write(finished_html)
+
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dir_path_content):
+        return
+    
+    dir_contents = os.listdir(dir_path_content)
+    for content in dir_contents:
+        content_src_path = os.path.join(dir_path_content, content)
+
+        if os.path.isdir(content_src_path):
+            new_dest_dir_path = os.path.join(dest_dir_path, content)
+            generate_page_recursive(content_src_path, template_path, new_dest_dir_path)
+        else:
+            dest_content = content.replace(".md", ".html")
+            content_dest_path = os.path.join(dest_dir_path, dest_content)
+
+            print(f"Generating page from {content_src_path} to {content_dest_path} using {template_path}")
+
+            with open(content_src_path) as m: md = m.read()
+            with open(template_path) as t: template = t.read()
+
+            html_node = markdown_to_html_node(md)
+            html_string = html_node.to_html()
+            title = extract_title(md)
+
+            new_html = template.replace("{{ Title }}", title)
+            finished_html = new_html.replace("{{ Content }}", html_string)
+
+            os.makedirs(os.path.dirname(content_dest_path), exist_ok=True)
+
+            with open(content_dest_path, "w") as f: f.write(finished_html)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 from copystatic import copy_contents
-from generatepage import generate_page
+from generatepage import generate_page_recursive
 
 def main():
     project_root = os.path.dirname(os.path.dirname(__file__))
@@ -8,27 +8,11 @@ def main():
     dest_dir = os.path.join(project_root, "public")
     copy_contents(src_dir, dest_dir, True)
 
-    gen_src = os.path.join(project_root,"content/index.md")
+    gen_src = os.path.join(project_root,"content")
     gen_tmp = os.path.join(project_root, "template.html")
-    gen_dest = os.path.join(project_root, "public/index.html")
+    gen_dest = os.path.join(project_root, "public")
 
-    glorfindel_src = os.path.join(project_root, "content/blog/glorfindel/index.md")
-    glorfindel_dest = os.path.join(project_root, "public/blog/glorfindel/index.html")
-
-    maj_src = os.path.join(project_root, "content/blog/majesty/index.md")
-    maj_dest = os.path.join(project_root, "public/blog/majesty/index.html")
-
-    tom_src = os.path.join(project_root, "content/blog/tom/index.md")
-    tom_dest = os.path.join(project_root, "public/blog/tom/index.html")
-
-    contact_src = os.path.join(project_root, "content/contact/index.md")
-    contact_dest = os.path.join(project_root, "public/contact/index.html")
-
-    generate_page(gen_src, gen_tmp, gen_dest)
-    generate_page(glorfindel_src, gen_tmp, glorfindel_dest)
-    generate_page(maj_src, gen_tmp, maj_dest)
-    generate_page(tom_src, gen_tmp, tom_dest)
-    generate_page(contact_src, gen_tmp, contact_dest)
+    generate_page_recursive(gen_src, gen_tmp, gen_dest)
 
 
 main()
